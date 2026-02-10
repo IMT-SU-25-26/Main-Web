3817be3ec0ae41b339f691d74d0f5c09de653c4a
Verzional <verzional777@gmail.com>
2026-02-10 23:42:13 +0700

    Proxy Init

---
+++ src/proxy.ts
@@ -0,0 +1,18 @@
+import { type NextRequest, NextResponse } from "next/server";
+import { getSessionCookie } from "better-auth/cookies";
+
+export function proxy(request: NextRequest) {
+  const sessionCookie = getSessionCookie(request);
+
+  const isProtectedRoute = request.nextUrl.pathname.startsWith("");
+
+  if (isProtectedRoute && !sessionCookie) {
+    return NextResponse.redirect(new URL("/sign-in", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
