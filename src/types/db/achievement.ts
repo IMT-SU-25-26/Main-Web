e0553a272944c15d068ef546b8cded38ca90bded
Verzional <verzional777@gmail.com>
2026-02-25 21:54:04 +0700

    Achievement Z Type

---
+++ src/types/db/achievement.ts
@@ -0,0 +1,18 @@
+import { z } from "zod";
+import { ImageSchema } from "./image";
+
+export const AchievementSchema = z.object({
+  title: z
+    .string()
+    .min(1, "Title is required")
+    .max(255, "Title must be at most 255 characters long"),
+  description: z.string().min(1, "Description is required"),
+  teamInfo: z
+    .string()
+    .min(1, "Team info is required")
+    .max(255, "Team info must be at most 255 characters long"),
+  featured: z.boolean().default(false),
+  images: z.array(ImageSchema).optional(),
+});
+
+export type Achievement = z.infer<typeof AchievementSchema>;
